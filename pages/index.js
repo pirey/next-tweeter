452ee95fb7cec6880ead5b5b712ed0b595535c81
Yeri <yeri@accelbyte.net>
2021-04-24 12:43:20 +0700

    signup form

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import ArticleCard from '../components/ArticleCard';
 import Chat from '../components/Chat';
 import ProfileCard from '../components/ProfileCard';
+import SignupForm from '../components/SignupForm';
 
 export default function Home() {
   return (
@@ -13,6 +14,9 @@ export default function Home() {
       </div>
       <div className="py-6 px-4">
         <ArticleCard />
+      </div>
+      <div className="py-6 px-4">
+        <SignupForm />
       </div>
     </div>
   );
